d650403cd471b4130bc3c559cfb8ddab94c6cb23
pathmann <thomas.pathmann@googlemail.com>
2016-08-02 13:26:15 +0200

    just a plugin to startup the plugin's configdialog

---
+++ testhelper.py
@@ -0,0 +1,20 @@
+from ts3plugin import ts3plugin, PluginHost
+
+
+class testhelper(ts3plugin):
+    name = "testhelper"
+    apiVersion = 20
+    requestAutoload = False
+    version = "1.0"
+    author = "Thomas \"PLuS\" Pathmann"
+    description = "Just a helper to test pyTSon"
+    offersConfigure = False
+    commandKeyword = ""
+    infoTitle = ""
+    menuItems = []
+    hotkeys = []
+    
+    
+    def __init__(self):
+        #keep in mind, that this plugin won't show as enabled on client startup
+        PluginHost.configure(None)
