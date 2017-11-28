0305ce579848d4848710bdb7705689f24be64c3f
Bluscream <admin@timo.de.vc>
2017-11-28 04:59:47 +0100

    :thinking:
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/pingHighScore/__init__.py
+++ scripts/pingHighScore/__init__.py
@@ -1,7 +1,6 @@
-import pytson, ts3lib, ts3defines, itertools, re, textwrap
+import pytson, ts3lib, ts3defines, itertools, re, textwrap, datetime
 from ts3plugin import ts3plugin
-from datetime import datetime
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 def take(n, iterable):
     """Return first n items of the iterable as a list"""
@@ -54,33 +53,54 @@ class pingHighScore(ts3plugin):
     def processCommand(self, schid, cmd):
         cmd = cmd.split(' ', 1)
         command = cmd[0].lower()
+        if command == "clear":
+            self.c = []; return 1
+        count = int(cmd[1])
         if command == "ping":
-            count = int(cmd[1]) if cmd[1] else 5
-            c = {}
-            (err, clids) = ts3lib.getClientList(schid)
-            for clid in clids:
-                # if len(self.c) > 10: break
-                if not clid in self.c:
-                    ts3lib.requestConnectionInfo(schid, clid)
-                    self.c.append(clid)
-                (err, ping) = ts3lib.getConnectionVariableAsUInt64(schid, clid, ts3defines.ConnectionProperties.CONNECTION_PING)
-                if err == ts3defines.ERROR_ok: c[clid] = ping
-            print(c)
-            s = sorted(c.items(), key=lambda x: int(x[1]))
-            t = take(count, s)
-            string = ""
-            place = 1
-            for k,v in t:
-                string += '{0}: {1} with [b]{2}[/b]ms\n'.format(place,clientURL(schid,k),v)
-                place += 1
-            (err, ownid) = ts3lib.getClientID(schid)
-            (err, ownchan) = ts3lib.getChannelOfClient(schid, ownid)
-            message = [string[i:i + 900] for i in range(0, len(string), 900)]
-            # message = re.findall('.{1,1024}(?:\n|$)', string)
-            # message = (textwrap.wrap(string, 1024))
-            # message = (line.strip() for line in re.findall(r'.{1,80}(?:\s+|$)', string))
-            # message = textwrap.wrap(string, 1024, break_long_words=False)
-            for msg in message: ts3lib.requestSendChannelTextMsg(schid, '\n{0}'.format(msg), ownchan)
-        elif command == "clear":
-            self.c = []
+            scores = self.getScores(schid, ts3defines.ConnectionProperties.CONNECTION_PING, 0, count)
+            self.sendScores(schid, scores, 0, "[b]Ping Top {0}:[/b]\n".format(count))
+        elif command == "time":
+            scores = self.getScores(schid, ts3defines.ConnectionProperties.CONNECTION_CONNECTED_TIME, 1, count)
+            self.sendScores(schid, scores, 1, "[b]Connection Time Top {0}:[/b]\n".format(count))
+        elif command == "idle":
+            scores = self.getScores(schid, ts3defines.ConnectionProperties.CONNECTION_IDLE_TIME, 1, count)
+            self.sendScores(schid, scores, 1, "[b]Idle Time Top {0}:[/b]\n".format(count))
         return 1
+
+    def processVar(self, var, mode):
+        if mode == 0:
+            return '[b]{0}[/b]ms'.format(round(var, 2))
+        elif mode == 1:
+            return '{0}'.format(datetime.timedelta(milliseconds=var)).split('.')[0]
+
+    def sendScores(self, schid, scores, mode, string):
+        place = 1
+        for k,v in scores:
+            string += '{place}: {clientURL} {val}\n'.format(place=place,clientURL=clientURL(schid,k),val=self.processVar(v, mode))
+            place += 1
+        (err, ownid) = ts3lib.getClientID(schid)
+        (err, ownchan) = ts3lib.getChannelOfClient(schid, ownid)
+        message = [string[i:i + 900] for i in range(0, len(string), 900)]
+        # message = re.findall('.{1,1024}(?:\n|$)', string)
+        # message = (textwrap.wrap(string, 1024))
+        # message = (line.strip() for line in re.findall(r'.{1,80}(?:\s+|$)', string))
+        # message = textwrap.wrap(string, 1024, break_long_words=False)
+        c = 0
+        for msg in message: ts3lib.requestSendChannelTextMsg(schid, '\n{0}'.format(msg) if c > 0 else msg, ownchan); c += 1
+
+    def getScores(self, schid, flag, mode, count=10):
+        c = {}
+        (err, clids) = ts3lib.getClientList(schid)
+        for clid in clids:
+            # if len(self.c) > 10: break
+            if not clid in self.c:
+                ts3lib.requestConnectionInfo(schid, clid)
+                self.c.append(clid)
+            (err, ping) = ts3lib.getConnectionVariable(schid, clid, flag)
+            if err == ts3defines.ERROR_ok: c[clid] = ping
+        print(c)
+        if mode == 0:
+            s = sorted(c.items(), key=lambda x: int(x[1]))
+        elif mode == 1:
+            s = sorted(c.items(), key=lambda x: int(x[1]), reverse=True)
+        return take(count, s)
