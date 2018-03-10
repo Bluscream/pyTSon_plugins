3d5b7233a83e8fa682ad87217a9c1447bba85afa
YourMum <yourmum@github.com>
2018-03-10 16:55:16 +0100

    kein bock mehr
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/countContacts/__init__.py
+++ scripts/countContacts/__init__.py
@@ -72,37 +72,84 @@ class countContacts(ts3plugin):
 
     def contactStats(self,detailed=False):
         buddies = 0;blocked = 0;neutral = 0;unknown = 0
-        female = 0;male = 0;soundboard = 0
-        f_buddies = 0;m_buddies = 0
-        f_blocked = 0;m_blocked = 0
-        f_neutral = 0;m_neutral = 0
-        f_unknown = 0;m_unknown = 0
+        counters = {};bcounters = {};blcounters = {};ncounters = {}; ucounters = {}
+        for k in self.cfg['filters']:
+            counters[k] = 0;bcounters[k] = 0;blcounters[k] = 0;ncounters[k] = 0;ucounters[k] = 0
         contacts = getContacts()
         if detailed:
             first = round(time.time()); last = 0
             firstc = None; lastc = None
         for contact in contacts:
-            nick = contact["Nickname"].decode("utf-8").lower()
-            if nick.startswith(self.cfg['filters']['Female Prefix']): female += 1
-            elif nick.startswith(self.cfg['filters']['Male Prefix']): male += 1
-            elif nick.startswith(self.cfg['filters']['Soundboard Prefix']): soundboard += 1
+            nick = contact["Nickname"].decode("utf-8")
+            for k in self.cfg['filters']:
+                tmp = self.cfg.get('filters', k).split('|', 2)
+                case = bool(int(tmp[0]));mode = tmp[1];text = tmp[2]
+                if case: nick = nick.lower(); text = text.lower()
+                if mode == "Prefix":
+                    if nick.startswith(text): counters[k] += 1
+                elif mode == "Suffix":
+                    if nick.endswith(text): counters[k] += 1
+                elif mode == "Contains":
+                    if text in nick: counters[k] += 1
+                elif mode == "Equals":
+                    if text == nick: counters[k] += 1
             status = contact["Friend"]
             if status == ContactStatus.FRIEND:
                 buddies += 1
-                if nick.startswith(self.cfg['filters']['Female Prefix']): f_buddies += 1
-                elif nick.startswith(self.cfg['filters']['Male Prefix']): m_buddies += 1
+                for k in self.cfg['filters']:
+                    tmp = self.cfg.get('filters', k).split('|', 2)
+                    case = bool(int(tmp[0]));mode = tmp[1];text = tmp[2]
+                    if case: nick = nick.lower(); text = text.lower()
+                    if mode == "Prefix":
+                        if nick.startswith(text): bcounters[k] += 1
+                    elif mode == "Suffix":
+                        if nick.endswith(text): bcounters[k] += 1
+                    elif mode == "Contains":
+                        if text in nick: bcounters[k] += 1
+                    elif mode == "Equals":
+                        if text == nick: bcounters[k] += 1
             elif status == ContactStatus.BLOCKED:
                 blocked += 1
-                if nick.startswith(self.cfg['filters']['Female Prefix']): f_blocked += 1
-                elif nick.startswith(self.cfg['filters']['Male Prefix']): m_blocked += 1
+                for k in self.cfg['filters']:
+                    tmp = self.cfg.get('filters', k).split('|', 2)
+                    case = bool(int(tmp[0]));mode = tmp[1];text = tmp[2]
+                    if case: nick = nick.lower(); text = text.lower()
+                    if mode == "Prefix":
+                        if nick.startswith(text): blcounters[k] += 1
+                    elif mode == "Suffix":
+                        if nick.endswith(text): blcounters[k] += 1
+                    elif mode == "Contains":
+                        if text in nick: blcounters[k] += 1
+                    elif mode == "Equals":
+                        if text == nick: blcounters[k] += 1
             elif status == ContactStatus.NEUTRAL:
                 neutral += 1
-                if nick.startswith(self.cfg['filters']['Female Prefix']): f_neutral += 1
-                elif nick.startswith(self.cfg['filters']['Male Prefix']): m_neutral += 1
+                for k in self.cfg['filters']:
+                    tmp = self.cfg.get('filters', k).split('|', 2)
+                    case = bool(int(tmp[0]));mode = tmp[1];text = tmp[2]
+                    if case: nick = nick.lower(); text = text.lower()
+                    if mode == "Prefix":
+                        if nick.startswith(text): ncounters[k] += 1
+                    elif mode == "Suffix":
+                        if nick.endswith(text): ncounters[k] += 1
+                    elif mode == "Contains":
+                        if text in nick: ncounters[k] += 1
+                    elif mode == "Equals":
+                        if text == nick: ncounters[k] += 1
             else:
                 unknown += 1
-                if nick.startswith(self.cfg['filters']['Female Prefix']): f_unknown += 1
-                elif nick.startswith(self.cfg['filters']['Male Prefix']): m_unknown += 1
+                for k in self.cfg['filters']:
+                    tmp = self.cfg.get('filters', k).split('|', 2)
+                    case = bool(int(tmp[0]));mode = tmp[1];text = tmp[2]
+                    if case: nick = nick.lower(); text = text.lower()
+                    if mode == "Prefix":
+                        if nick.startswith(text): ucounters[k] += 1
+                    elif mode == "Suffix":
+                        if nick.endswith(text): ucounters[k] += 1
+                    elif mode == "Contains":
+                        if text in nick: ucounters[k] += 1
+                    elif mode == "Equals":
+                        if text == nick: ucounters[k] += 1
             if "LastSeenEpoch" in contact and detailed:
                 if contact["LastSeenEpoch"] < first:
                     first = contact["LastSeenEpoch"]
@@ -111,17 +158,22 @@ class countContacts(ts3plugin):
                     last = contact["LastSeenEpoch"]
                     lastc = contact
         sum = buddies+blocked+neutral+unknown
-        msg = ["My Contact Stats:"]
-        msg.append("Sum: [b]{}[/b] | [color=purple]Female[/color]: {} ({}%) | [color=lightblue]Male[/color]: {} ({}%) | Others: {} ({}%){}".format(
-                    sum, female, percentage(female, sum), male, percentage(male, sum), sum-(male+female), percentage(sum-(male+female), sum), " | Soundboard: {} ({}%)".format(soundboard, percentage(soundboard, sum)) if soundboard > 0 else ""))
-        msg.append("[color=green]Buddies[/color]: [b]{}[/b] ({}%) | [color=purple]Female[/color]: {} ({}%) | [color=lightblue]Male[/color]: {} ({}%) | Others: {} ({}%)".format(
-                    buddies, percentage(buddies, sum), f_buddies, percentage(f_buddies, buddies), m_buddies, percentage(m_buddies, buddies), buddies-(m_buddies+f_buddies), percentage(buddies-(m_buddies+f_buddies), buddies)))
-        msg.append("[color=red]Blocked[/color]: [b]{}[/b] ({}%) | [color=purple]Female[/color]: {} ({}%) | [color=lightblue]Male[/color]: {} ({}%) | Others: {} ({}%)".format(
-                    blocked, percentage(blocked, sum), f_blocked, percentage(f_blocked, blocked), m_blocked, percentage(m_blocked, blocked), blocked-(m_blocked+f_blocked), percentage(blocked-(m_blocked+f_blocked), blocked)))
-        msg.append("Neutral: [b]{}[/b] ({}%) | [color=purple]Female[/color]: {} ({}%) | [color=lightblue]Male[/color]: {} ({}%) | Others: {} ({}%)".format(
-                    neutral, percentage(neutral, sum), f_neutral, percentage(f_neutral, neutral), m_neutral, percentage(m_neutral, neutral), neutral-(m_neutral+f_neutral), percentage(neutral-(m_neutral+f_neutral), neutral)))
-        if unknown > 0: msg.append("Unknown: [color=orange]{}[/color] ({}%) | [color=purple]Female[/color]: {} ({}%) | [color=lightblue]Male[/color]: {} ({}%) | Others: {} ({}%)".format(
-                    unknown, percentage(unknown, sum), f_unknown, percentage(f_unknown, unknown), m_unknown, percentage(m_unknown, unknown), unknown-(m_unknown+f_unknown), percentage(unknown-(m_unknown+f_unknown), unknown)))
+        msg = ["[u]My Contact Stats[/u]:"]
+        tmp = []
+        for c in counters: tmp.append("{}: {} ({}%)".format(c.title(), counters[c], percentage(counters[c], sum)))
+        msg.append("Sum: [b]{}[/b] {}".format(sum, " | ".join(tmp)))
+        tmp = []
+        for c in bcounters: tmp.append("{}: {} ({}%)".format(c.title(), bcounters[c], percentage(bcounters[c], sum)))
+        msg.append("[color=green]Buddies[/color]: [b]{}[/b] ({}%) {}".format(buddies, percentage(buddies, sum), " | ".join(tmp)))
+        tmp = []
+        for c in blcounters: tmp.append("{}: {} ({}%)".format(c.title(), blcounters[c], percentage(blcounters[c], sum)))
+        msg.append("[color=red]Blocked[/color]: [b]{}[/b] ({}%) {}".format(blocked, percentage(blocked, sum), " | ".join(tmp)))
+        tmp = []
+        for c in ncounters: tmp.append("{}: {} ({}%)".format(c.title(), ncounters[c], percentage(ncounters[c], sum)))
+        msg.append("Neutral: [b]{}[/b] ({}%) {}".format(neutral, percentage(neutral, sum), " | ".join(tmp)))
+        tmp = []
+        for c in ucounters: tmp.append("{}: {} ({}%)".format(c.title(), ucounters[c], percentage(ucounters[c], sum)))
+        msg.append("[color=orange]Unknown[/color]: [b]{}[/b] ({}%) {}".format(unknown, percentage(unknown, sum), " | ".join(tmp)))
         if detailed and first:
             msg.append("First: {} {} {} on {}".format(
             firstc["LastSeen"].replace("T", " "), self.readableContactStatus(firstc), clientURL(1, 0, firstc["IDS"], firstc["Nickname"].decode("utf-8", "ignore")), channelURL(1, 0, firstc["LastSeenServerName"].decode("utf-8", "ignore"))))
@@ -205,9 +257,9 @@ class contactStatsFilters(QDialog):
                 chk_case.setChecked(bool(int(tmp[0])))
                 self.tbl_filters.setCellWidget(pos, 0, chk_case)
                 box_type = QComboBox()
-                box_type.setCurrentText(tmp[1])
                 box_type.addItems(["Prefix", "Suffix", "Contains", "Equals"])
                 box_type.connect("currentIndexChanged(int)", self.currentIndexChanged)
+                box_type.setCurrentText(tmp[1])
                 self.tbl_filters.setCellWidget(pos, 1, box_type)
                 self.tbl_filters.setItem(pos, 2, QTableWidgetItem(o))
                 self.tbl_filters.setItem(pos, 3, QTableWidgetItem(tmp[2]))
@@ -222,3 +274,15 @@ class contactStatsFilters(QDialog):
         # print("item:", item)
         # self.tbl_members.at
         # ts3lib.requestSetClientChannelGroup(self.schid, [item.itemData], [self.channel], [self.dbid])
+
+    def on_btn_apply_clicked(self):
+        items = []
+        for i in range(self.tbl_filters.rowCount):
+             uid = self.lst_active.item(i)
+             items.append(uid)
+        self.grp_active.setTitle("{} Active".format(self.lst_active.count))
+        self.cfg.set('general', 'badges', ','.join(items))
+        self.setCustomBadges()
+
+    def on_btn_save_clicked(self):
+        with open(self.ini, 'w') as configfile: self.cfg.write(configfile)
