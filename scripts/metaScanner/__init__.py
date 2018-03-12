22a537190e13d43af98e70e41146043e22ec7962
Bluscream <admin@timo.de.vc>
2018-03-12 19:32:02 +0100

    kein bock mehr
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/metaScanner/__init__.py
+++ scripts/metaScanner/__init__.py
@@ -13,7 +13,7 @@ class metaScanner(ts3plugin):
     author = "Bluscream"
     description = "Let's you scan the subscribed channels for users that have metaData set"
     offersConfigure = False
-    commandKeyword = None
+    commandKeyword = "meta"
     infoTitle = None
     menuItems = [(ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 0, name, "")]
     hotkeys = []
@@ -21,15 +21,25 @@ class metaScanner(ts3plugin):
     def __init__(self):
         if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
 
+    def processCommand(self, schid, command): self.printMetaUsers(schid); return True
+
     def onMenuItemEvent(self, schid, atype, menuItemID, selectedItemID):
-        if atype != ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL or menuItemID != 0: return
+        if atype == ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL and menuItemID == 0: self.printMetaUsers(schid)
+
+    def onClientMoveEvent(self, schid, clientID, oldChannelID, newChannelID, visibility, moveMessage):
+        if oldChannelID != 0: return
+        (err, mdata) = ts3lib.getClientVariable(schid, clientID, ts3defines.ClientProperties.CLIENT_META_DATA)
+        if not mdata or mdata.strip() == "": return
+        ts3lib.printMessage(schid, "{} {}'s Metadata: {}".format(timestamp(), clientURL(clientID), mdata), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
+
+    def printMetaUsers(self, schid):
         (err, clist) = ts3lib.getClientList(schid)
         msg = []
         for c in clist:
             (err, mdata) = ts3lib.getClientVariable(schid, c, ts3defines.ClientProperties.CLIENT_META_DATA)
-            if mdata and mdata.strip() != "":
-                mdata = (mdata[:100].replace("\n", "\\n"))
-                msg.append("{}: {}".format(clientURL(schid, c), mdata))
+            if not mdata or mdata.strip() == "": continue
+            mdata = (mdata[:100].replace("\n", "\\n"))
+            msg.append("{}: {}".format(clientURL(schid, c), mdata))
         ts3lib.printMessageToCurrentTab("{}[u]Online users with metadata[/u]: [b]{}[/b]".format(timestamp(), len(msg)))
         try: msg.sort(key=lambda s: s.split(": ")[1])
         except: pass
