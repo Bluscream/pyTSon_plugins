4a6679bb5b2337518aa3b4f316b5c81d465f8d94
Bluscream <admin@timo.de.vc>
2018-03-12 16:51:51 +0100

    kein bock mehr
    Signed-off-by: Bluscream <admin@timo.de.vc>

---
+++ scripts/metaScanner/__init__.py
@@ -0,0 +1,32 @@
+from ts3plugin import ts3plugin, PluginHost
+from bluscream import *
+from pytson import getPluginPath, getCurrentApiVersion
+from traceback import format_exc
+import ts3defines, ts3lib, ts3client
+
+class metaScanner(ts3plugin):
+    name = "Scan Meta Data"
+    try: apiVersion = getCurrentApiVersion()
+    except: apiVersion = 21
+    requestAutoload = True
+    version = "1"
+    author = "Bluscream"
+    description = "Let's you scan the subscribed channels for users that have metaData set"
+    offersConfigure = False
+    commandKeyword = None
+    infoTitle = None
+    menuItems = [(ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 0, name, "")]
+    hotkeys = []
+
+    def __init__(self):
+        if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
+
+    def onMenuItemEvent(self, schid, atype, menuItemID, selectedItemID):
+        if atype != ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL or menuItemID != 0: return
+        ts3lib.printMessageToCurrentTab("{}[u]Online users with metadata[/u]:".format(timestamp()))
+        (err, clist) = ts3lib.getClientList(schid)
+        for c in clist:
+            (err, mdata) = ts3lib.getClientVariable(schid, c, ts3defines.ClientProperties.CLIENT_META_DATA)
+            if mdata and mdata.strip() != "":
+                mdata = (mdata[:100].replace("\\", "\\\\"))
+                ts3lib.printMessageToCurrentTab("{}: {}".format(clientURL(schid, c), mdata))
