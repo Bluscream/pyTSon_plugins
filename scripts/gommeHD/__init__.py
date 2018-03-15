4ae771b15fb61c462333fbcd7a7789c0ca5bfcbc
Bluscream <admin@timo.de.vc>
2018-03-15 18:03:15 +0100

    kein bock mehr
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/gommeHD/__init__.py
+++ scripts/gommeHD/__init__.py
@@ -1,8 +1,9 @@
 import pytson, ts3lib, ts3defines
-from ts3plugin import ts3plugin
+from ts3plugin import ts3plugin, PluginHost
 from datetime import datetime
 from PythonQt.QtCore import QTimer
 from collections import defaultdict
+from bluscream import timestamp, getContactStatus, ContactStatus
 
 class gommeHD(ts3plugin):
     name = "GommeHD nifty tricks"
@@ -16,19 +17,21 @@ class gommeHD(ts3plugin):
     infoTitle = None
     hotkeys = []
     menuItems = [
+        (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 0, "Ask for avatar", ""),
         (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_CHANNEL, 0, "Send Steam", ""),
         (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_CLIENT, 0, "Send Steam", "")
     ]
-    debug = False
     suid = "QTRtPmYiSKpMS8Oyd4hyztcvLqU="
     channelAdminGroupID = 10
     gommeBotNick = "Gomme-Bot"
-    msg = "um nur Personen ab dem ausgewählen Rang die Möglichkeit zu geben, in deinen Channel zu joinen."
-    blockMSG = "Diesen Befehl kannst du nur als Channel-Admin ausführen!"
-    welcomeMSG = ['Gomme-Bot geöffnet! Tippe "ruhe", um den Ruhe-Rang zu erhalten!','Du möchtest nicht mehr angeschrieben werden? Tippe "togglebot"']
     delay = 1500
     settings = { "maxclients": 10, "tp": 23 }
     violations = defaultdict(int)
+    askForAvatar = False
+    alreadyAsked = []
+    msg = "um nur Personen ab dem ausgewählen Rang die Möglichkeit zu geben, in deinen Channel zu joinen."
+    blockMSG = "Diesen Befehl kannst du nur als Channel-Admin ausführen!"
+    welcomeMSG = ['Gomme-Bot geöffnet! Tippe "ruhe", um den Ruhe-Rang zu erhalten!','Du möchtest nicht mehr angeschrieben werden? Tippe "togglebot"']
     steammsg = """
 Steam: [url]https://steamcommunity.com/profiles/76561198022446661[/url]
 Add as friend: [url]steam://friends/add/76561198022446661[/url]
@@ -36,15 +39,23 @@ Common games: [url]https://steamcommunity.com/profiles/76561198022446661/games/?
 Account Value: [url]https://steamdb.info/calculator/76561198022446661/?cc=eu[/url]
 Trade URL: [url]https://steamcommunity.com/tradeoffer/new/?partner=62180933&token=fSMYHMGM[/url]
 """
-
-    @staticmethod
-    def timestamp(): return '[{:%Y-%m-%d %H:%M:%S}] '.format(datetime.now())
+    avatarmsg = """Hey {nick}, es wäre voll knorke von dir wenn du mir erlauben würdest dein Premium zu benutzen damit ich mir nen avatar setzen kann.
+Dabei gehst du keine scam Gefahr ein weil du mir das jederzeit wieder wegnehmen kannst auch wenn ich schon disconnected bin (frag jeden das ist so)
+Ich erklär dir auch wie's geht:
+1. Geh auf den Gomme minecraft server und gib [color=red]/ts set {myuid}[/color] ein und schreib mir in TS deinen minecraft namen.
+2. Ich werde deinen mc namen dem gomme bot schreiben damit er mir premium geben kann
+3. Wenn ich dann Premium habe werde ich mir kurz nen avatar setzen max. 2 min
+4. Dann sag ich dir bescheid und du gibst in minecraft [color=green]/ts set {uid}[/color] ein und hast dein premium wieder
+5. Ich werde mich bei dir bedanken ;) 
+"""
 
     def __init__(self):
-        if self.debug: ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(self.timestamp(),self.name,self.author))
+        if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(),self.name,self.author))
 
     def onMenuItemEvent(self, schid, atype, menuItemID, selectedItemID):
         if menuItemID != 0: return
+        if atype == ts3defines.PluginItemType.PLUGIN_SERVER:
+            self.askForAvatar = not self.askForAvatar
         if atype == ts3defines.PluginItemType.PLUGIN_CHANNEL:
             ts3lib.requestSendChannelTextMsg(schid, self.steammsg, selectedItemID)
         elif atype == ts3defines.PluginItemType.PLUGIN_CLIENT:
@@ -62,31 +73,37 @@ Trade URL: [url]https://steamcommunity.com/tradeoffer/new/?partner=62180933&toke
     def sendMessage(self):
         ts3lib.requestSendPrivateTextMsg(self.schid, "registriert", self.gommeBotID)
 
+    def onClientMoveEvent(self, schid, clientID, oldChannelID, newChannelID, visibility, moveMessage):
+        if not self.askForAvatar: return
+        (err, suid) = ts3lib.getServerVariable(schid, ts3defines.VirtualServerProperties.VIRTUALSERVER_UNIQUE_IDENTIFIER)
+        if suid != self.suid: return
+        if oldChannelID != 0: return
+        (err, uid) = ts3lib.getClientVariable(schid, clientID, ts3defines.ClientProperties.CLIENT_UNIQUE_IDENTIFIER)
+        if getContactStatus(uid) == ContactStatus.BLOCKED: return
+        if uid in self.alreadyAsked: return
+        (err, sgroups) = ts3lib.getClientVariable(schid, clientID, ts3defines.ClientPropertiesRare.CLIENT_SERVERGROUPS)
+        if 17 in sgroups: return
+        if set(sgroups).isdisjoint([31,30,14]): return
+        (err, myuid) = ts3lib.getClientSelfVariable(schid, ts3defines.ClientProperties.CLIENT_UNIQUE_IDENTIFIER)
+        (err, nick) = ts3lib.getClientVariable(schid, clientID, ts3defines.ClientProperties.CLIENT_NICKNAME)
+        ts3lib.requestSendPrivateTextMsg(schid, self.avatarmsg.replace("{nick}", nick).replace("{myuid}", myuid).replace("{uid}", uid), clientID)
+        self.alreadyAsked.append(uid)
+
     def onClientChannelGroupChangedEvent(self, schid, channelGroupID, channelID, clientID, invokerClientID, invokerName, invokerUniqueIdentity):
         (err, suid) = ts3lib.getServerVariable(schid, ts3defines.VirtualServerProperties.VIRTUALSERVER_UNIQUE_IDENTIFIER)
-        # if self.debug: ts3lib.printMessageToCurrentTab('suid != self.suid: {}'.format(suid != self.suid))
         if suid != self.suid: return
         (err, ownID) = ts3lib.getClientID(schid)
-        # if self.debug: ts3lib.printMessageToCurrentTab('clientID != ownID: {}'.format(clientID != ownID))
         if clientID != ownID: return
-        # if self.debug: ts3lib.printMessageToCurrentTab('channelGroupID != self.channelAdminGroupID: {}'.format(channelGroupID != self.channelAdminGroupID))
         if channelGroupID != self.channelAdminGroupID: return
-        # if self.debug: ts3lib.printMessageToCurrentTab('invokerClientID != 0: {}'.format(invokerClientID != 0))
         if invokerClientID == 0:
             (err, ntp) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NEEDED_TALK_POWER)
             if not ntp or ntp == 0: ts3lib.setChannelVariableAsInt(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NEEDED_TALK_POWER, self.settings["tp"])
-            # (err, cmc) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelProperties.CHANNEL_MAXCLIENTS)
             ts3lib.setChannelVariableAsInt(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_FLAG_MAXCLIENTS_UNLIMITED, 0)
             ts3lib.setChannelVariableAsInt(schid, channelID, ts3defines.ChannelProperties.CHANNEL_MAXCLIENTS, self.settings["maxclients"])
             (err, cnp) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NAME_PHONETIC)
             if not cnp or cnp == "": ts3lib.setChannelVariableAsString(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NAME_PHONETIC, "Team | Lounge 1")
             ts3lib.flushChannelUpdates(schid, channelID)
         return
-        # clid = ts3lib.getClientIDbyNickname(schid, self.gommeBotNick)
-        # if not clid: ts3lib.printMessage(schid, 'Gomme-Bot not found.', ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
-        # (err, uid) = ts3lib.getClientVariable(schid, self.gommeBotID, ts3defines.ClientProperties.CLIENT_UNIQUE_IDENTIFIER)
-        # if uid != "serveradmin": return
-        # if self.debug: ts3lib.printMessageToCurrentTab('self.gommeBotID == 0: {}'.format(self.gommeBotID == 0))
         if self.gommeBotID == 0: return
         ts3lib.requestSendPrivateTextMsg(schid, "registriert", self.gommeBotID)
 
@@ -94,32 +111,26 @@ Trade URL: [url]https://steamcommunity.com/tradeoffer/new/?partner=62180933&toke
         (err, suid) = ts3lib.getServerVariable(schid, ts3defines.VirtualServerProperties.VIRTUALSERVER_UNIQUE_IDENTIFIER)
         if suid != self.suid: return
         (err, ownID) = ts3lib.getClientID(schid)
-        if self.debug: ts3lib.printMessageToCurrentTab('invokerID == ownID: {}'.format(invokerID == ownID))
         if invokerID == ownID:
             (err, self.settings["maxclients"]) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelProperties.CHANNEL_MAXCLIENTS)
             (err, self.settings["tp"]) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NEEDED_TALK_POWER)
         (err, ownChannel) = ts3lib.getChannelOfClient(schid, ownID)
-        if self.debug: ts3lib.printMessageToCurrentTab('channelID != ownChannel: {}'.format(channelID != ownChannel))
         if channelID != ownChannel: return
         (err, invokerChannel) = ts3lib.getChannelOfClient(schid, invokerID)
-        if self.debug: ts3lib.printMessageToCurrentTab('invokerChannel == channelID: {}'.format(invokerChannel == channelID))
         if invokerChannel == channelID: return
         _needed = False
         (err, ntp) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NEEDED_TALK_POWER)
-        if self.debug: ts3lib.printMessageToCurrentTab('ntp != self.settings["tp"]: {}'.format(ntp != self.settings["tp"]))
         if ntp != self.settings["tp"]:
             _needed = True
             ts3lib.setChannelVariableAsInt(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_NEEDED_TALK_POWER, self.settings["tp"])
         (err, cmc) = ts3lib.getChannelVariable(schid, channelID, ts3defines.ChannelProperties.CHANNEL_MAXCLIENTS)
         ts3lib.setChannelVariableAsInt(schid, channelID, ts3defines.ChannelPropertiesRare.CHANNEL_FLAG_MAXCLIENTS_UNLIMITED, 0)
-        if self.debug: ts3lib.printMessageToCurrentTab('cmc != self.settings["maxclients"]: {}'.format(cmc != self.settings["maxclients"]))
         if cmc != self.settings["maxclients"]:
             _needed = True
             ts3lib.setChannelVariableAsInt(schid, channelID, ts3defines.ChannelProperties.CHANNEL_MAXCLIENTS, self.settings["maxclients"])
         if _needed:
             ts3lib.flushChannelUpdates(schid, channelID)
             self.violations[invokerUniqueIdentifier] += 1
-            if self.debug: ts3lib.printMessageToCurrentTab("violations of {}: {}".format(invokerUniqueIdentifier, self.violations[invokerUniqueIdentifier]))
             if self.violations[invokerUniqueIdentifier] > 2:
                 (err, dbid) = ts3lib.getClientVariable(schid, ts3defines.ClientPropertiesRare.CLIENT_DATABASE_ID)
                 ts3lib.requestSetClientChannelGroup(schid, [9], [channelID], [dbid])
