f6b03adc28006fb510288cc345f50be0f249463a
Bluscream <admin@timo.de.vc>
2018-04-16 05:24:18 +0200

    :thinking: Combined the pluginCMD scripts into one and added some syntax help
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/pluginCMD/__init__.py
+++ scripts/pluginCMD/__init__.py
@@ -1,7 +1,9 @@
 from ts3plugin import ts3plugin
 from datetime import datetime
 from urllib.parse import quote as urlencode
-import ts3defines, ts3lib
+from bluscream import timestamp, sendCommand, clientURL
+from traceback import format_exc
+import ts3defines, ts3lib, _ts3lib
 
 class pluginCMD(ts3plugin):
     name = "Plugin CMD sender / viewer"
@@ -17,46 +19,36 @@ class pluginCMD(ts3plugin):
     hotkeys = []
     debug = False
 
-    @staticmethod
-    def timestamp(): return '[{:%Y-%m-%d %H:%M:%S}] '.format(datetime.now())
-
     def __init__(self):
-        if self.debug: ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(self.timestamp(),self.name,self.author))
+        if self.debug: ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(),self.name,self.author))
 
-    def clientURL(self, schid=None, clid=0, uid=None, nickname=None, encodednick=None):
-        if schid == None:
-            try: schid = ts3lib.getCurrentServerConnectionHandlerID()
-            except: pass
-        if uid == None:
-            try: (error, uid) = ts3lib.getClientVariable(schid, clid, ts3defines.ClientProperties.CLIENT_UNIQUE_IDENTIFIER)
-            except: pass
-        if nickname == None:
-            try: (error, nickname) = ts3lib.getClientVariable(schid, clid, ts3defines.ClientProperties.CLIENT_NICKNAME)
-            except: nickname = uid
-        if encodednick == None:
-            try: encodednick = urlencode(nickname)
-            except: pass
-        return "[url=client://{0}/{1}~{2}]{3}[/url]".format(clid, uid, encodednick, nickname)
-
-    def processCommand(self, schid, cmd):
-        cmd = cmd.split(' ', 1)
-        targetmode = cmd[0].lower()
-        target = []
-        command = cmd[1]
-        if targetmode in ["chan", "channel", "0"]:
-            targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_CURRENT_CHANNEL
-        elif targetmode in ["serv", "server", "1"]:
-            targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_SERVER
-        elif targetmode in ["cli", "client", "2"]:
-            targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_CLIENT
+    def processCommand(self, schid, fullcmd):
+        try:
+            cmd = fullcmd.split(' ', 1)
+            mode = cmd[0].lower()
             _cmd = cmd[1].split(' ', 1)
-            target = [int(_cmd[0])]
+            targetmode = _cmd[0].lower()
             command = _cmd[1]
-        elif targetmode in ["chansub", "channelsubscribed", "3"]:
-            targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_CURRENT_CHANNEL_SUBSCRIBED_CLIENTS
-        ts3lib.sendPluginCommand(schid, command, targetmode, target)
-        return True
+            target = []
+            if targetmode in ["chan", "channel", "0"]:
+                targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_CURRENT_CHANNEL
+            elif targetmode in ["serv", "server", "1"]:
+                targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_SERVER
+            elif targetmode in ["cli", "client", "2"]:
+                targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_CLIENT
+                _cmd = command[1].split(' ', 1)
+                target = [int(_cmd[0])]
+                command = _cmd[1]
+            elif targetmode in ["chansub", "channelsubscribed", "3"]:
+                targetmode = ts3defines.PluginTargetMode.PluginCommandTarget_CURRENT_CHANNEL_SUBSCRIBED_CLIENTS
+            if mode in ["raw", "r", "true", "1"]: _ts3lib.sendPluginCommand(schid, command, targetmode, target)
+            else: ts3lib.sendPluginCommand(schid, command, targetmode, target)
+            return True
+        except:
+            ts3lib.printMessageToCurrentTab("Syntax: [b]/py cmd <raw> <channel,server,channelsubscribed,client <clid>> <cmd>")
+            ts3lib.logMessage("Error while processing \"{}\"\n{}".format(fullcmd, format_exc()), ts3defines.LogLevel.LogLevel_WARNING, self.name, schid)
+
 
     def onPluginCommandEvent(self, schid, clid, pluginCommand):
         ts3lib.printMessageToCurrentTab("onPluginCommandEvent")
-        ts3lib.printMessage(schid, "{0} PluginMessage from {1}: {2}".format(self.timestamp(), self.clientURL(clid), pluginCommand), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
+        ts3lib.printMessage(schid, "{0} PluginMessage from {1}: {2}".format(timestamp(), clientURL(clid), pluginCommand), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
