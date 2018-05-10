6bd7582b9894544443fcd076291bd13604d30032
Bluscream <admin@timo.de.vc>
2018-05-10 23:02:27 +0200

    :thinking:
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/customDisconnect/__init__.py
+++ scripts/customDisconnect/__init__.py
@@ -1,4 +1,5 @@
 import ts3lib, ts3defines
+from ts3defines import ConnectStatus
 from ts3plugin import ts3plugin, PluginHost
 from pytson import getCurrentApiVersion
 from PythonQt.Qt import QApplication
@@ -16,8 +17,8 @@ class customDisconnect(ts3plugin):
     commandKeyword = "disconnect"
     infoTitle = None
     menuItems = [
-        (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 0, "Disconnect", ""),
-        (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 1, "Close Tab", "")
+        (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 0, "Disconnect", "scripts/%s/disconnect.svg"%__name__),
+        (ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL, 1, "Close Tab", "scripts/%s/tab_close_button.svg"%__name__)
     ]
     hotkeys = [
         ("disconnect", "Prompts for disconnect message before disconnecting"),
@@ -31,16 +32,27 @@ class customDisconnect(ts3plugin):
         if atype != ts3defines.PluginMenuType.PLUGIN_MENU_TYPE_GLOBAL: return
         self.disconnect(schid, True if mID == 1 else False)
 
+    def onConnectStatusChangeEvent(self, schid, newStatus, errorNumber): self.checkServer(schid, newStatus)
+    def currentServerConnectionChanged(self, schid): self.checkServer(schid, ts3lib.getConnectionStatus(schid))
+    def checkServer(self, schid, status):
+        if status == ts3defines.ConnectStatus.STATUS_CONNECTION_ESTABLISHED: status = True
+        elif status == ts3defines.ConnectStatus.STATUS_DISCONNECTED: status = False
+        else: return
+        for menuItem in self.menuItems:
+            try: ts3lib.setPluginMenuEnabled(PluginHost.globalMenuID(self, menuItem[1]), status)
+            except: pass
+
+
     def processCommand(self, schid, keyword): self.onHotkeyOrCommandEvent(keyword, schid)
-
     def onHotkeyEvent(self, keyword): self.onHotkeyOrCommandEvent(keyword)
-
     def onHotkeyOrCommandEvent(self, keyword, schid=0):
         if keyword == "disconnect": self.disconnect(schid)
         elif keyword == "close_tab": self.disconnect(schid, True)
 
     def disconnect(self, schid=0, close_tab=False):
         if schid < 1: schid = ts3lib.getCurrentServerConnectionHandlerID()
+        status = ts3lib.getConnectionStatus(schid)
+        if status != ts3defines.ConnectStatus.STATUS_CONNECTION_ESTABLISHED: return
         clipboard = QApplication.clipboard().text()
         msg = inputBox("Disconnect", "Message", clipboard)
         ts3lib.stopConnection(schid, msg if msg else "")
