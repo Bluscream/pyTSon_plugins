e57c91093882073884664f1073709a65a52a0df2
Bluscream <admin@timo.de.vc>
2018-08-17 16:58:26 +0200

    % added additional checks to autoProxy
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/autoProxy/__init__.py
+++ scripts/autoProxy/__init__.py
@@ -2,6 +2,7 @@ import ts3lib, ts3defines
 from ts3plugin import ts3plugin, PluginHost
 from pytson import getCurrentApiVersion
 from PythonQt.QtCore import QTimer
+from PythonQt.QtNetwork import QHostAddress
 from bluscream import timestamp
 from ts3cloud_proxy import TS3CloudProxy
 
@@ -10,7 +11,7 @@ class autoProxy(ts3plugin):
     try: apiVersion = getCurrentApiVersion()
     except: apiVersion = 21
     requestAutoload = True
-    version = "1.1"
+    version = "1.2"
     author = "Bluscream"
     description = "Uses ts3.cloud's ts3proxy service to switch to a proxy on every connection.\nRequires ts3cloud_proxy.py in your include folder!"
     offersConfigure = False
@@ -23,6 +24,7 @@ class autoProxy(ts3plugin):
     port = 9987
 
     nickname = "Bluscream"
+    whitelist = ["127.0.0.1","homeserver","minopia.de"] # all lower case
 
     def __init__(self):
         if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
@@ -30,10 +32,18 @@ class autoProxy(ts3plugin):
     def onConnectStatusChangeEvent(self, schid, newStatus, errorNumber):
         if newStatus != ts3defines.ConnectStatus.STATUS_CONNECTING: return
         if self.proxied: self.proxied = False; return
-        err, self.host, self.port, self.pw = ts3lib.getServerConnectInfo(schid)
-        address = "{}:{}".format(self.host,self.port)
+        err, host, port, pw = ts3lib.getServerConnectInfo(schid)
+        if host.lower() in self.whitelist: ts3lib.printMessageToCurrentTab("[color=green]%s is whitelisted, not using proxy!" % host); return
+        ip = QHostAddress(host)
+        if not ip.isNull():
+            if ip.isLoopback(): ts3lib.printMessageToCurrentTab("[color=green]%s is Loopback, not using proxy!" % host); return
+            elif ip.isMulticast(): ts3lib.printMessageToCurrentTab("[color=green]%s is Multicast, not using proxy!" % host); return
+            elif ip.isLinkLocal(): ts3lib.printMessageToCurrentTab("[color=green]%s is LinkLocal, not using proxy!" % host); return
+            # elif ip.isInSubnet(): ts3lib.printMessageToCurrentTab("[color=green]%s is LinkLocal, not using proxy!" % host); return # TODO: Check aginst local subnet
+        address = "{}:{}".format(host,port)
         ts3lib.printMessageToCurrentTab("[color=red]Not proxied on %s, disconnecting!"%address)
         ts3lib.stopConnection(schid, "switching to proxy")
+        self.host = host;self.port = port;self.pw = pw
         QTimer.singleShot(250, self.proxy)
 
     def proxy(self):
