fef88ec78bbca18b9977339df56e4c92683df633
Bluscream <admin@timo.de.vc>
2018-08-17 18:21:02 +0200

    % removed debug stuff
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/autoProxy/__init__.py
+++ scripts/autoProxy/__init__.py
@@ -29,13 +29,6 @@ class autoProxy(ts3plugin):
     whitelist = ["127.0.0.1","192.168.2.38","192.168.2.39"] # all lower case
 
     def __init__(self):
-        # self.request.setUrl(QUrl("https://www.ts3.cloud/ts3proxy"))
-        # self.request.setHeader(QNetworkRequest.UserAgentHeader, "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0")
-        # self.request.setHeader(QNetworkRequest.ContentTypeHeader, "application/x-www-form-urlencoded")
-        # self.request.setRawHeader(QByteArray("Accept"),QByteArray("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"))
-        # self.request.setRawHeader(QByteArray("Accept-Language"),QByteArray("en-US,en;q=0.5"))
-        # self.request.setRawHeader(QByteArray("Referer"),QByteArray("https://www.ts3.cloud/ts3proxy"))
-        # self.request.setRawHeader(QByteArray("Pragma"),QByteArray("no-cache"))
         self.nwmc.connect("finished(QNetworkReply*)", self.reply)
         if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
 
@@ -48,8 +41,6 @@ class autoProxy(ts3plugin):
         if not ip.isNull():
             if ip.isLoopback(): ts3lib.printMessageToCurrentTab("[color=green]%s is Loopback, not using proxy!" % host); return
             elif ip.isMulticast(): ts3lib.printMessageToCurrentTab("[color=green]%s is Multicast, not using proxy!" % host); return
-            # elif ip.isLinkLocal(): ts3lib.printMessageToCurrentTab("[color=green]%s is LinkLocal, not using proxy!" % host); return
-            # elif ip.isInSubnet(): ts3lib.printMessageToCurrentTab("[color=green]%s is LAN IP, not using proxy!" % host); return # TODO: Check aginst local subnet
         address = "{}:{}".format(host,port)
         ts3lib.printMessageToCurrentTab("[color=red]Not proxied on %s, disconnecting!"%address)
         ts3lib.stopConnection(schid, "switching to proxy")
@@ -57,8 +48,6 @@ class autoProxy(ts3plugin):
         print(err, nick)
         self.pw = pw
         payload = self.payload.format(host=host,port=port)
-        # postDataSize = QByteArray.number(len(payload))
-        # self.request.setRawHeader("Content-Length", postDataSize)
         self.nwmc.post(self.request, payload)
 
     def reply(self, reply):
