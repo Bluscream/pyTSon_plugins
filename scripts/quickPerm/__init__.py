92f3adfcbf29d11aa4e5e4c043f7cf0f9aa0dcee
Bluscream <admin@timo.de.vc>
2018-12-05 02:23:23 +0100

    % temp
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/quickPerm/__init__.py
+++ scripts/quickPerm/__init__.py
@@ -21,50 +21,19 @@ class quickPerm(ts3plugin):
     menuItems = []
     hotkeys = []
     retcodes = {}
-    perms = {} # "success": 0, "fail": 0, "processed": 0, "total": 0, "stop": False
+    perms = {} # "success": 0, "fail": 0, "processed": 0, "stop": False
     flood = False
-    perms_essential = []
-    perms_full = []
-    permissions = [
-        ("b_client_ignore_antiflood", True, True),
-        ("i_permission_modify_power", 75, True),
-        ("i_client_permission_modify_power", 75, True),
-        ("i_group_modify_power", 75, True),
-        ("i_channel_permission_modify_power", 75, True),
-        ("i_group_member_add_power", 75, True),
-        ("b_virtualserver_token_add", True, True),
-        ("b_virtualserver_token_list", True, True),
-        ("b_virtualserver_token_use", True, True),
-        ("b_virtualserver_token_delete", True, True),
-        ("b_client_ignore_bans", True, True),
-        ("i_client_ban_power", 75, True),
-        ("i_client_needed_ban_power", 75, True),
-        ("b_client_remoteaddress_view", True, True),
-        ("b_client_create_modify_serverquery_login", True, True),
-        ("b_virtualserver_servergroup_permission_list", True, True),
-        ("b_virtualserver_channelgroup_permission_list", True, True),
-        ("b_virtualserver_client_permission_list", True, True),
-        ("b_virtualserver_channel_permission_list", True, True),
-        ("b_virtualserver_channelclient_permission_list", True, True)
-    ]
 
     def __init__(self):
-        with open(path.join(self.path, "essential.csv")) as csvfile:
-            reader = csv.reader(csvfile)
-            for row in reader:
-                if row['Colour'] == 'blue':
-                    print(row['ID'] ,row ['Make'],row ['Colour'])
         if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
 
     def processCommand(self, schid, keyword):
         perm = keyword.split(' ')
-        print(len(perm), perm)
         # if len(perm) < 2: return False
         # if len(perm) == 2: perm.append(False).append(False)
         skip = perm[2] if len(perm) > 2 else False
         val = boolean(perm[1]) if len(perm) > 1 else True
         perm = perm[0]
-        print(perm, val, skip)
         (err, clid) = ts3lib.getClientID(schid)
         (err, cldbid) = ts3lib.getClientVariable(schid, clid, ClientPropertiesRare.CLIENT_DATABASE_ID)
         (err, pid) = ts3lib.getPermissionIDByName(schid, perm)
@@ -83,34 +52,41 @@ class quickPerm(ts3plugin):
         if sgid == dgid: return
         self.getPerms(schid, clientID, sgid)
 
-    def getPerms(self, schid, clid, sgid = 0):
+    def getPerms(self, schid, clid = 0, sgid = 0):
         if self.flood: return
+        if not clid: (err, clid) = ts3lib.getClientID(schid)
         (err, cldbid) = ts3lib.getClientVariable(schid, clid, ClientPropertiesRare.CLIENT_DATABASE_ID)
-        self.perms = { "success": 0, "fail": 0, "processed": 0, "total": len(self.permissions), "stop": False}
-        for perm in self.permissions:
-            if self.perms["stop"]: break
-            """
-            if self.perms["fail"] > 0:
-                self.perms = { "success": 0, "fail": 0, "processed": 0, "total": 0}; self.retcodes = {}
-                ts3lib.printMessage(schid, "{} failed :(".format(self.name), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
-                break
-            """
-            (err, pid) = ts3lib.getPermissionIDByName(schid, perm[0])
-            retcode = ts3lib.createReturnCode()
-            self.retcodes[retcode] = perm[0]
-            v = 100 if sgid == 2 and perm[1] == 75 else perm[1]
-            ts3lib.requestClientAddPerm(schid, cldbid, [pid], [v], [perm[2]], retcode)
-            (err, ownCID) = ts3lib.getChannelOfClient(schid, clid)
-            if self.perms["stop"]: break
-            ts3lib.requestChannelClientAddPerm(schid, ownCID, cldbid, [pid], [v], retcode)
-            # ts3lib.requestChannelAddPerm(schid, ownCID, [pid], [v], "quickperm")
+        self.perms = { "success": 0, "fail": 0, "processed": 0, "stop": False}
+        with open(path.join(self.path, "essential.csv")) as csvfile:
+            permissions = csv.reader(csvfile, delimiter=';')
+            for perm in permissions:
+                if self.perms["stop"]: break
+                (err, pid) = ts3lib.getPermissionIDByName(schid, perm[0])
+                retcode = ts3lib.createReturnCode()
+                self.retcodes[retcode] = perm[0]
+                v = 100 if sgid == 2 and perm[1] == "75" else int(perm[1])
+                ts3lib.requestClientAddPerm(schid, cldbid, [pid], [v], [int(perm[2])], retcode)
+                (err, ownCID) = ts3lib.getChannelOfClient(schid, clid)
+                if self.perms["stop"]: break
+                ts3lib.requestChannelClientAddPerm(schid, ownCID, cldbid, [pid], [v], retcode)
+                # ts3lib.requestChannelAddPerm(schid, ownCID, [pid], [v], "quickperm")
+
+    def getFullPerms(self, schid):
+        (err, clid) = ts3lib.getClientID(schid)
+        (err, cldbid) = ts3lib.getClientVariable(schid, clid, ClientPropertiesRare.CLIENT_DATABASE_ID)
+        with open(path.join(self.path, "full.csv")) as csvfile:
+            perms_full = csv.reader(csvfile)
+            for perm in perms_full:
+                (err, pid) = ts3lib.getPermissionIDByName(schid, perm[0])
+                ts3lib.requestClientAddPerm(schid, cldbid, [pid], [int(perm[1])], [int(perm[2])])
 
     def disableFlood(self): self.flood = False
 
     def onServerErrorEvent(self, schid, errorMessage, error, returnCode, extraMessage):
         if not returnCode in self.retcodes: return False
         reason = "finished"; self.perms["processed"] += 1
-        if error == ERROR_ok: self.perms["success"] += 1
+        if error == ERROR_ok:
+            self.perms["success"] += 1
         elif error == ERROR_client_is_flooding:
             self.perms["fail"] += 1; self.perms["stop"] = True;reason = "failed because of flooding"
         elif error in [ERROR_permissions_client_insufficient,ERROR_permissions_insufficient_group_power,ERROR_permissions_insufficient_permission_power]: self.perms["fail"] += 1
@@ -120,6 +96,7 @@ class quickPerm(ts3plugin):
         if not len(self.retcodes):
             ts3lib.printMessage(schid, "{} {} {}".format(self.name, reason, self.perms), PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
             if self.perms["stop"]: self.flood = True; QTimer.singleShot(5000, self.disableFlood)
+            elif self.perms["success"] > 0: self.getFullPerms(schid)
         return True
 """
     def onServerPermissionErrorEvent(self, schid, errorMessage, error, returnCode, failedPermissionID):
--- scripts/silentError/__init__.py
+++ scripts/silentError/__init__.py
@@ -1,23 +1,38 @@
+# coding=utf8
 from ts3plugin import ts3plugin, PluginHost
 from bluscream import timestamp
-import ts3defines, ts3lib
+from datetime import timedelta
+from PythonQt.QtCore import QTimer
+import ts3defines, ts3lib, re
 
 class silentError(ts3plugin):
-    name = "Silent Errors"
+    name = "Anti Flood Timer"
     apiVersion = 21
-    requestAutoload = False
+    requestAutoload = True
     version = "1"
     author = "Bluscream"
-    description = "Silences TeaSpeak's \"not implemented\" errors"
+    description = ""
     offersConfigure = False
     commandKeyword = ""
     infoTitle = None
     menuItems = []
     hotkeys = []
+    regex = r"^retry in (\d+)ms$"
+    timers = {}
 
     def __init__(self):
+        self.regex = re.compile(self.regex)
         if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
 
     def onServerErrorEvent(self, schid, errorMessage, error, returnCode, extraMessage):
-        return True
+        if error == ts3defines.ERROR_client_is_flooding:
+            # print(self.name,"onServerErrorEvent", schid, errorMessage, error, returnCode, extraMessage)
+            wait = int(self.regex.match(extraMessage).group(1))
+            self.timers[schid] = (wait, QTimer)
+            self.timers[schid][1].singleShot(wait, self.floodOver)
+            d = timedelta(milliseconds=wait)
+            ts3lib.printMessage(schid, "{} [color=red]Remaining Time: [b]{}[/b]".format(self.name, str(d)), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
 
+    def floodOver(self):
+        cur = self.timers.popitem()
+        ts3lib.printMessage(cur[0], "{} [color=green][b]expired!".format(self.name), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
