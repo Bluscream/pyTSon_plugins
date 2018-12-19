04a61fb0091f6375a01d11da898a671fa77ae872
Bluscream <admin@timo.de.vc>
2018-12-19 20:06:17 +0100

    % gay LOL but works for python plugins
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/aaa_ts3Ext/__init__.py
+++ scripts/aaa_ts3Ext/__init__.py
@@ -1,8 +1,9 @@
-import ts3lib, ts3defines
+import ts3lib
 from ts3plugin import ts3plugin, PluginHost
 from pytson import getCurrentApiVersion
-from bluscream import timestamp, GroupType
+from bluscream import timestamp, GroupType, log
 from ts3Ext import ts3SessionHost, logLevel
+from ts3defines import *
 
 class aaa_ts3Ext(ts3plugin):
     name = "aaa_ts3Ext"
@@ -13,7 +14,7 @@ class aaa_ts3Ext(ts3plugin):
     author = "Bluscream"
     description = "ts3Ext library implementation"
     offersConfigure = False
-    commandKeyword = ""
+    commandKeyword = "ext"
     infoTitle = None
     menuItems = []
     hotkeys = []
@@ -28,23 +29,38 @@ class aaa_ts3Ext(ts3plugin):
         for schid in schids:
             (err, status) = ts3lib.getConnectionStatus(schid)
             self.onConnectStatusChangeEvent(schid, status, err)
-        if PluginHost.cfg.getboolean("general", "verbose"):
-            i = 1
-            for active in PluginHost.active: print(i, active); i += 1
-            ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
+        log(self, LogLevel.LogLevel_DEBUG, "{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author), 0)
+
+    def processCommand(self, schid, keyword):
+            args = keyword.split(' ')
+            cmd = args.pop(0).lower()
+            if cmd == "dump":
+                print(self.tabs)
+            else: return False
+            return True
 
     def onConnectStatusChangeEvent(self, schid, status, errorNumber):
-        if status == ts3defines.ConnectStatus.STATUS_CONNECTION_ESTABLISHED:
-            if not schid in self.tabs:
-                self.tabs[schid] = {
-                    "channelBanGroup": None,
-                    "channelModGroup": None
-                }
+        if status == ConnectStatus.STATUS_CONNECTION_ESTABLISHED:
             srv = self.ts3host.getServer(schid)
-            srv.requestServerGroupList() # ts3lib.requestChannelGroupList(schid)
-            srv.requestChannelGroupList() # ts3lib.requestServerGroupList(schid)
-        elif status == ts3defines.ConnectStatus.STATUS_DISCONNECTED:
-            if schid in self.tabs: del self.tabs[schid]
+            self.tabs[schid] = {
+                "channelBanGroup": None,
+                "channelModGroup": None,
+                "srv": srv,
+            }
+            err, self.tabs[schid]["name"] = ts3lib.getServerVariable(schid, VirtualServerProperties.VIRTUALSERVER_NAME)
+            err, self.tabs[schid]["host"], self.tabs[schid]["port"], self.tabs[schid]["pw"] = ts3lib.getServerConnectInfo(schid)
+            self.tabs[schid]["address"] = '{}:{}'.format(self.tabs[schid]["host"], self.tabs[schid]["port"]) if hasattr(self.tabs[schid], 'port') else self.tabs[schid]["host"]
+            err, self.tabs[schid]["clid"] = ts3lib.getClientID(schid)
+            err, self.tabs[schid]["nick"] = ts3lib.getClientSelfVariable(schid, ClientProperties.CLIENT_NICKNAME) # ts3lib.getClientVariable(schid, self.tabs[schid]["clid"], ClientProperties.CLIENT_NICKNAME)
+            err, self.tabs[schid]["nick_phonetic"] = ts3lib.getClientSelfVariable(schid, ClientPropertiesRare.CLIENT_NICKNAME_PHONETIC)
+            err, self.tabs[schid]["uid"] = ts3lib.getClientSelfVariable(schid, ClientProperties.CLIENT_UNIQUE_IDENTIFIER)
+            err, self.tabs[schid]["token"] = ts3lib.getClientSelfVariable(schid, ClientPropertiesRare.CLIENT_DEFAULT_TOKEN)
+            err, self.tabs[schid]["cid"] = ts3lib.getChannelOfClient(schid, self.tabs[schid]["clid"])
+            err, self.tabs[schid]["cpath"], self.tabs[schid]["cpw"] = ts3lib.getChannelConnectInfo(schid, self.tabs[schid]["cid"])
+            srv.requestServerGroupList()
+            srv.requestChannelGroupList()
+        elif status == ConnectStatus.STATUS_DISCONNECTED:
+            if schid in self.tabs: self.tabs[schid]["status"] = status
 
     def onServerGroupListEvent(self, schid, serverGroupID, name, atype, iconID, saveDB):
         if atype != GroupType.REGULAR: return
@@ -53,22 +69,42 @@ class aaa_ts3Ext(ts3plugin):
 
     def onChannelGroupListEvent(self, schid, channelGroupID, name, atype, iconID, saveDB):
         if atype != GroupType.REGULAR: return
-        debug = PluginHost.cfg.getboolean("general", "verbose")
         tab = self.tabs[schid]
         for _name in self.banned_names:
             if _name in name.upper():
                 tab["channelBanGroup"] = channelGroupID
-                if debug: print(schid, 'channelBanGroup', tab["channelBanGroup"])
                 break
         for _name in self.mod_names:
             if _name in name.upper():
                 tab["channelModGroup"] = channelGroupID
-                if debug: print(schid, 'channelModGroup', tab["channelModGroup"])
                 break
         srv = self.ts3host.getServer(schid)
         srv.updateChannelGroup(channelGroupID, name, iconID)
 
     def onTalkStatusChangeEvent(self, schid, status, isReceivedWhisper, clientID):
         self.ts3host.getServer(schid).updateTalkStatus(clientID, status, isReceivedWhisper)
+
+    def onClientMoveEvent(self, schid, clientID, oldChannelID, newChannelID, visibility, moveMessage):
+        if schid not in self.tabs: return
+        if clientID != self.tabs[schid]["clid"]: return
+        self.tabs[schid]["cid"] = newChannelID
+        (err, self.tabs[schid]["cpath"], self.tabs[schid]["cpw"]) = ts3lib.getChannelConnectInfo(schid, newChannelID)
+
+    def onClientSelfVariableUpdateEvent(self, schid, flag, oldValue, newValue):
+        if schid not in self.tabs: return
+        if flag == ClientProperties.CLIENT_NICKNAME:
+            self.tabs[schid]["nick"] = newValue
+        elif flag == ClientPropertiesRare.CLIENT_NICKNAME_PHONETIC:
+            self.tabs[schid]["nick_phonetic"] = newValue
+        elif flag == ClientProperties.CLIENT_UNIQUE_IDENTIFIER:
+            self.tabs[schid]["uid"] = newValue
+        elif flag == ClientPropertiesRare.CLIENT_DEFAULT_TOKEN:
+            self.tabs[schid]["token"] = newValue
+
+    def onPacketOut(self, msg, schid):
+        if schid in self.tabs:
+            if msg.startswith("clientinit "):
+                self.tabs[schid]["clientinit"] = msg
+        return False, msg
 
     def stop(self): del self.ts3host
