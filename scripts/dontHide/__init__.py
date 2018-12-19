e3244355017ef35d38c34a726ca5b99edce38c92
Bluscream <admin@timo.de.vc>
2018-12-19 12:42:13 +0100

    % gay LOL but works for python plugins
    Signed-off-by: Bluscream <admin@timo.de.vc>

--- scripts/dontHide/__init__.py
+++ scripts/dontHide/__init__.py
@@ -3,6 +3,7 @@ from ts3plugin import ts3plugin, PluginHost
 from pytson import getCurrentApiVersion
 from PythonQt.QtCore import QTimer
 from bluscream import timestamp, intList, getContactStatus, ContactStatus, sendCommand
+# from ts3Ext import ts3SessionHost as ts3host
 
 class dontHide(ts3plugin):
     name = "Don't Hide"
@@ -14,7 +15,7 @@ class dontHide(ts3plugin):
     description = "Shows clients that hide."
     offersConfigure = False
     commandKeyword = ""
-    infoTitle = None
+    infoTitle = " "
     menuItems = []
     hotkeys = []
     timer = QTimer()
@@ -24,21 +25,50 @@ class dontHide(ts3plugin):
         if "aaa_ts3Ext" in PluginHost.active:
             ts3ext = PluginHost.active["aaa_ts3Ext"]
             self.ts3host = ts3ext.ts3host
-            del self.timer
+            ts3lib.logMessage("{}: Dependency loaded".format(self.name), ts3defines.LogLevel.LogLevel_WARNING, "pyTSon", 0)
         else:
             retry = 1000
             self.timer.singleShot(retry, self.__init__)
-            ts3lib.printMessageToCurrentTab("{}: [color=red]Dependency not yet loaded, retrying in {} second(s)!".format(self.name, retry/1000))
+            ts3lib.logMessage("{}: Dependency not yet loaded, retrying in {} second(s)!".format(self.name, retry/1000), ts3defines.LogLevel.LogLevel_WARNING, "pyTSon", 0)
             return
         if PluginHost.cfg.getboolean("general", "verbose"): ts3lib.printMessageToCurrentTab("{0}[color=orange]{1}[/color] Plugin for pyTSon by [url=https://github.com/{2}]{2}[/url] loaded.".format(timestamp(), self.name, self.author))
 
     def stop(self):
         if hasattr(self, "timer"):
             if self.timer.isActive(): self.timer.stop()
-            del self.timer
+
+    def onConnectStatusChangeEvent(self, schid, newStatus, errorNumber):
+        if newStatus != ts3defines.ConnectStatus.STATUS_CONNECTION_ESTABLISHED: return
+        counts = self.getClientCounts(schid)
+        if counts["hidden"]["total"] > 0:
+            ts3lib.printMessage(schid, "[color=orange][b]{}[/b] hidden users on this server!".format(counts["hidden"]["total"]), ts3defines.PluginMessageTarget.PLUGIN_MESSAGE_TARGET_SERVER)
+
+    def getClientCounts(self, schid):
+        ret = {"total": {}, "visible": {}, "hidden": {}}
+        visible = self.ts3host.getServer(schid).users
+        ret["visible"]["users"] = 0
+        ret["visible"]["queries"] = 0
+        for user in visible:
+            (err, ctype) = ts3lib.getClientVariable(schid, user.clientID, ts3defines.ClientPropertiesRare.CLIENT_TYPE)
+            if ctype == ts3defines.ClientType.ClientType_NORMAL: ret["visible"]["users"] += 1
+            elif ctype == ts3defines.ClientType.ClientType_SERVERQUERY: ret["visible"]["queries"] += 1
+        ret["visible"]["total"] = ret["visible"]["users"]+ret["visible"]["queries"] # len(visible)
+        (err, ret["total"]["clients"]) = ts3lib.getServerVariable(schid, ts3defines.VirtualServerProperties.VIRTUALSERVER_CLIENTS_ONLINE)
+        (err, ret["total"]["queries"]) = ts3lib.getServerVariable(schid, ts3defines.VirtualServerPropertiesRare.VIRTUALSERVER_QUERYCLIENTS_ONLINE)
+        (err, ret["max"]) = ts3lib.getServerVariable(schid, ts3defines.VirtualServerProperties.VIRTUALSERVER_MAXCLIENTS)
+        ret["hidden"]["total"] = ret["total"]["clients"]-ret["visible"]["total"]
+        return ret
+
+    def infoData(self, schid, aid, atype):
+        if atype != ts3defines.PluginItemType.PLUGIN_SERVER: return
+        counts = self.getClientCounts(schid)
+        # if counts["hidden"]["total"] > 0:
+        return ["Shown Clients: %s"%counts["visible"]["users"], "Shown Queries: %s"%counts["visible"]["queries"], "Hidden: %s"%counts["hidden"]["total"], "{}".format(counts)]
+        # return None # ["clients:%s"%clients, "queries:%s"%queries, "max:%s"%max, "visible_users:%s"%visible_users, "visible_queries:%s"%visible_queries]
 
     def onClientMoveEvent(self, schid, clid, oldChannelID, newChannelID, visibility, moveMessage):
         if not newChannelID: return
+        return
         if visibility == ts3defines.Visibility.LEAVE_VISIBILITY:
             cmd = "notifycliententerview"
             client = self.buildClient(schid, clid, oldChannelID, newChannelID)
